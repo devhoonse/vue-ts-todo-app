8a2637d00b3934e8e1f0d76286ef8b2e0853643c
devhoonse <devhoonse@gmail.com>
2022-07-25 23:13:02 +0900

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /.html$/,
+          loader: 'vue-template-loader',
+          exclude: /index.html/,
+        },
+      ],
+    },
+  },
+});
